62312b787d26ab303bd08769df38e6e1ec35d125
Stephanie Norton <steph.jane.norton@gmail.com>
2020-10-02 08:20:41 -0600

    Add method  to add new toDo items to state

---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+// alt, instead of { useState }, could access method w. React.useState() 
+import './App.css';
+import ToDo from './ToDo'
+import ToDoForm from './ToDoForm'
+
+
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    // first parameter 'todos' is the name of your state
+    // second parameter 'setToDos' is what you use to set the state
+    // useState is what React uses to hook into the lifecycle of the component 
+    { text: "Book vacation to Hawaii" },
+    { text: "Hike a fourteener" },
+    { text: "Learn React Hooks" }
+  ])
+
+  const addToDo = (text) => {
+    const newTodos = [...todos, { text }]
+    setTodos(newTodos)
+  }
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <ToDo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+        <ToDoForm addToDo={addToDo} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
