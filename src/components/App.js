c3f262c522dc666fdc6a977b37e937148e6d3d2b
Stephanie Norton <steph.jane.norton@gmail.com>
2020-10-02 08:43:00 -0600

    Add useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // alt, instead of { useState }, could access method w. React.useState() 
 import './App.css';
 import ToDo from './ToDo'
@@ -7,6 +7,7 @@ import ToDoForm from './ToDoForm'
 
 
 const App = () => {
+  const [count, setCount] = useState(3)
   const [todos, setTodos] = useState([
     // first parameter 'todos' is the name of your state
     // second parameter 'setToDos' is what you use to set the state
@@ -19,25 +20,33 @@ const App = () => {
       isCompleted: false  }
   ])
 
-  const addToDo = (text) => {
+    const addToDo = (text) => {
     const newTodos = [...todos, { text }]
     setTodos(newTodos)
+    setCount(count + 1)
   }
 
   const completeToDo = index => {
     const newTodos = [...todos]
     newTodos[index].isCompleted = true
     setTodos(newTodos)
+    setCount(count - 1)
   }
 
   const removeToDo = index => {
     const newTodos = [...todos];
     newTodos.splice(index, 1)
     setTodos(newTodos)
+    setCount(count - 1)
   }
+
+  useEffect(() => {
+    document.title = `${count} active tasks!`
+  })
 
   return (
     <div className="app">
+      <h1>Your Todo List!</h1>
       <div className="todo-list">
         {todos.map((todo, index) => (
           <ToDo
